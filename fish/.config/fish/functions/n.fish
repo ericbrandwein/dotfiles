f3ab75d2cac2ad76403649e065f26da16b5bacbd
Eric Brandwein <brandweineric@gmail.com>
2025-01-25 13:12:30 -0300

    keyboard and wallpaper fixes

--- fish/.config/fish/functions/n.fish
+++ fish/.config/fish/functions/n.fish
@@ -27,7 +27,7 @@ function n --description 'support nnn quit and change directory'
     # stty lwrap undef
     # stty lnext undef
 
-    nnn $argv
+    nnn -A -i -n $argv
 
     if test -e $NNN_TMPFILE
         source $NNN_TMPFILE
